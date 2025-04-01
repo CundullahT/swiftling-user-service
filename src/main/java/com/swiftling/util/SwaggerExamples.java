d7c9bc03209c5b5169019a98d8ce216539ba7a37
CundullahT-CT <cterzioglu@cybertekschool.com>
2025-04-01 13:19:04 +0200

    fixes SwaggerExamples messages

--- src/main/java/com/swiftling/util/SwaggerExamples.java
+++ src/main/java/com/swiftling/util/SwaggerExamples.java
@@ -118,63 +118,59 @@ public class SwaggerExamples {
             "    }\n" +
             "  }\n" +
             "}";
-
+    //Done
     public static final String ADMIN_USER_GET_RESPONSE_LIST_EXAMPLE = "{\n" +
             "  \"success\": true,\n" +
             "  \"statusCode\": \"OK\",\n" +
             "  \"message\": \"Users are successfully retrieved.\",\n" +
             "  \"data\": [\n" +
             "    {\n" +
-            "      \"firstName\": \"John\",\n" +
-            "      \"lastName\": \"Doe\",\n" +
-            "      \"userName\": \"john.doe@example.com\",\n" +
-            "      \"phone\": \"1234567890\",\n" +
+            "      \"firstname\": \"John\",\n" +
+            "      \"lastname\": \"Doe\",\n" +
+            "      \"username\": \"john.doe\",\n" +
+            "      \"email\": \"john.doe@example.com\",\n" +
             "      \"enabled\": true,\n" +
             "      \"role\": {\n" +
-            "        \"description\": \"Employee\"\n" +
-            "      },\n" +
-            "      \"gender\": \"MALE\"\n" +
+            "        \"description\": \"Admin\"\n" +
+            "      }\n" +
             "    },\n" +
             "    {\n" +
-            "      \"firstName\": \"Jane\",\n" +
-            "      \"lastName\": \"Doe\",\n" +
-            "      \"userName\": \"jane.doe@example.com\",\n" +
-            "      \"phone\": \"9876543210\",\n" +
+            "      \"firstname\": \"Jane\",\n" +
+            "      \"lastname\": \"Doe\",\n" +
+            "      \"username\": \"jane.doe\",\n" +
+            "      \"email\": \"jane.doe@example.com\",\n" +
             "      \"enabled\": true,\n" +
             "      \"role\": {\n" +
-            "        \"description\": \"Manager\"\n" +
-            "      },\n" +
-            "      \"gender\": \"FEMALE\"\n" +
+            "        \"description\": \"Admin\"\n" +
+            "      }\n" +
             "    }\n" +
             "  ]\n" +
             "}";
-
+    //Done
     public static final String REGULAR_USER_GET_RESPONSE_LIST_EXAMPLE = "{\n" +
             "  \"success\": true,\n" +
             "  \"statusCode\": \"OK\",\n" +
             "  \"message\": \"Users are successfully retrieved.\",\n" +
             "  \"data\": [\n" +
             "    {\n" +
-            "      \"firstName\": \"John\",\n" +
-            "      \"lastName\": \"Doe\",\n" +
-            "      \"userName\": \"john.doe@example.com\",\n" +
-            "      \"phone\": \"1234567890\",\n" +
+            "      \"firstname\": \"John\",\n" +
+            "      \"lastname\": \"Doe\",\n" +
+            "      \"username\": \"john.doe\",\n" +
+            "      \"email\": \"john.doe@example.com\",\n" +
             "      \"enabled\": true,\n" +
             "      \"role\": {\n" +
-            "        \"description\": \"Employee\"\n" +
-            "      },\n" +
-            "      \"gender\": \"MALE\"\n" +
+            "        \"description\": \"User\"\n" +
+            "      }\n" +
             "    },\n" +
             "    {\n" +
-            "      \"firstName\": \"Jane\",\n" +
-            "      \"lastName\": \"Doe\",\n" +
-            "      \"userName\": \"jane.doe@example.com\",\n" +
-            "      \"phone\": \"9876543210\",\n" +
+            "      \"firstname\": \"Jane\",\n" +
+            "      \"lastname\": \"Doe\",\n" +
+            "      \"username\": \"jane.doe\",\n" +
+            "      \"email\": \"jane.doe@example.com\",\n" +
             "      \"enabled\": true,\n" +
             "      \"role\": {\n" +
-            "        \"description\": \"Manager\"\n" +
-            "      },\n" +
-            "      \"gender\": \"FEMALE\"\n" +
+            "        \"description\": \"User\"\n" +
+            "      }\n" +
             "    }\n" +
             "  ]\n" +
             "}";
@@ -193,14 +189,14 @@ public class SwaggerExamples {
             "  \"httpStatus\": \"CONFLICT\",\n" +
             "  \"localDateTime\": \"2024-01-01T00:00:00.0000000\"\n" +
             "}";
-
+    //Done
     public static final String USER_NOT_FOUND_RESPONSE_EXAMPLE = "{\n" +
             "  \"success\": false,\n" +
             "  \"message\": \"User does not exist.\",\n" +
             "  \"httpStatus\": \"NOT_FOUND\",\n" +
             "  \"localDateTime\": \"2024-01-01T00:00:00.0000000\"\n" +
             "}";
-
+    //Done
     public static final String USER_NOT_DELETED_RESPONSE_EXAMPLE = "{\n" +
             "  \"success\": false,\n" +
             "  \"message\": \"User can not be deleted.\",\n" +
@@ -208,26 +204,6 @@ public class SwaggerExamples {
             "  \"localDateTime\": \"2024-01-01T00:00:00.0000000\"\n" +
             "}";
 
-    public static final String PROJECT_COUNT_NOT_RETRIEVED_RESPONSE_EXAMPLE = "{\n" +
-            "  \"success\": false,\n" +
-            "  \"message\": \"Project count cannot be retrieved.\",\n" +
-            "  \"httpStatus\": \"INTERNAL_SERVER_ERROR\",\n" +
-            "  \"localDateTime\": \"2024-01-01T00:00:00.0000000\"\n" +
-            "}";
-
-    public static final String TASK_COUNT_NOT_RETRIEVED_RESPONSE_EXAMPLE = "{\n" +
-            "  \"success\": false,\n" +
-            "  \"message\": \"Task count cannot be retrieved.\",\n" +
-            "  \"httpStatus\": \"INTERNAL_SERVER_ERROR\",\n" +
-            "  \"localDateTime\": \"2024-01-01T00:00:00.0000000\"\n" +
-            "}";
-
-    public static final String ROLE_NOT_FOUND_RESPONSE_EXAMPLE = "{\n" +
-            "  \"success\": false,\n" +
-            "  \"message\": \"Role does not exist.\",\n" +
-            "  \"httpStatus\": \"NOT_FOUND\",\n" +
-            "  \"localDateTime\": \"2024-01-01T00:00:00.0000000\"\n" +
-            "}";
     //Done
     public static final String VALIDATION_EXCEPTION_RESPONSE_EXAMPLE = "{\n" +
             "  \"success\": false,\n" +
