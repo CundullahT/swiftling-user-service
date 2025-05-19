0593270246ea41c4f3fa4229024dcb04c7d85c83
CundullahT-CT <cterzioglu@cybertekschool.com>
2025-05-19 18:59:08 +0200

    enables feign client

--- src/main/java/com/swiftling/UserServiceApplication.java
+++ src/main/java/com/swiftling/UserServiceApplication.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -13,6 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class UserServiceApplication {
